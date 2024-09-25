9e01c51aaf5a15ec68fcaa669a25b0c99e8f3c57
krewdreele <keele.drew00@gmail.com>
2024-09-25 12:38:06 -0600

    Moved tweeter starter files (plus milestone 1) to github

---
+++ tweeter-web-starter/tweeter-web/src/components/authentication/AuthenticationField.tsx
@@ -0,0 +1,37 @@
+interface Props {
+    submit: (event: React.KeyboardEvent<HTMLElement>) => void;
+    setAlias: (alias: string) => void;
+    setPassword: (password: string) => void;
+}
+
+const AuthenticationField = ({submit, setAlias, setPassword}: Props) => {
+    return (
+        <>
+        <div className="form-floating">
+          <input
+            type="text"
+            className="form-control"
+            size={50}
+            id="aliasInput"
+            placeholder="name@example.com"
+            onKeyDown={submit}
+            onChange={(event) => setAlias(event.target.value)}
+          />
+          <label htmlFor="aliasInput">Alias</label>
+        </div>
+        <div className="form-floating">
+          <input
+            type="password"
+            className="form-control"
+            id="passwordInput"
+            placeholder="Password"
+            onKeyDown={submit}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <label htmlFor="passwordInput">Password</label>
+        </div>
+        </>
+    );
+}
+
+export default AuthenticationField;
